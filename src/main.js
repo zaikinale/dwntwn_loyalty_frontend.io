107dbf3260d0cd7affe59bcf416ecbb2b1cc76a3
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-10-11 22:47:57 +0300

    some_fix_adress

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Глобальные настройки
-window.API_BASE = "https://neat-ties-open.loca.lt"
+window.API_BASE = "https://itchy-moose-exist.loca.lt"
 
 createApp(App).mount('#app')
