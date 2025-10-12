d839878e8eec1484c0ff7012cf663a5a3e309ef3
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-10-12 23:51:20 +0300

    fix_adress_n

--- src/main 2.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// Глобальные настройки
-window.API_BASE = "http://localhost:8000"
-
-createApp(App).mount('#app')
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Глобальные настройки
-window.API_BASE = "http://localhost:8000"
+window.API_BASE = "https://dwntwn-loyalty-backend.onrender.com"
 
 createApp(App).mount('#app')
