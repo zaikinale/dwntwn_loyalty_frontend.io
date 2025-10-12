11400e37a748f5048e5c7310e49f89e00ab0069b
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-10-12 11:17:49 +0300

    some_fix_adress_base

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Глобальные настройки
-window.API_BASE = "https://itchy-moose-exist.loca.lt"
+window.API_BASE = "https://eight-rockets-matter.loca.lt"
 
 createApp(App).mount('#app')
