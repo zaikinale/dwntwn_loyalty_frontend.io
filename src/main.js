c67d53b902cc5348a090ab7640fe6d834ee82248
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-10-12 11:59:40 +0300

    fix_adress_4

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Глобальные настройки
-window.API_BASE = " https://old-pugs-hang.loca.lt"
+window.API_BASE = "https://ninety-clowns-glow.loca.lt"
 
 createApp(App).mount('#app')
