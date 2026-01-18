8bcaee47ee88d4330a51049ef6678b8d45741092
Alexei Zaikin <zaikinaleks04@mail.ru>
2026-01-18 14:08:18 +0300

    fix form show in select client

--- src/components/Admin/AdminView.vue
+++ src/components/Admin/AdminView.vue
@@ -714,7 +714,7 @@ const redeemGift = async () => {
 const selectClient = (client) => {
   newStaff.value.telegram_id = client.telegram_id
   newStaff.value.name = `${client.first_name} ${client.last_name}`
-  isFormExpanded.value = false
+  isFormExpanded.value = true
 }
 
 const addStaff = async () => {
